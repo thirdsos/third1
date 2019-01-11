7c381d440234187eb87684e75f118ee3958bc124
lab303-14 <lab303-14@labeng.local>
2019-01-11 18:12:09 +0700

    บันทึกตัวเลข

---
+++ lab3_5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string text1,text2;
+    int N;
+    int count;
+    cout<<"Enter the first text:";
+    cin>> text1 ;
+    cout<<"Enter the second text:";
+    cin>> text2 ;
+    cout<<"Enter N:";
+    cin>>N;
+    count=0;
+    while(count<N){
+    if(count==0){
+        cout<< " ";
+    }
+    
+    else{
+
+        cout<< " ";
+    }
+    
+    count=count+1;
+    }
+return 0;
+
+
+}
